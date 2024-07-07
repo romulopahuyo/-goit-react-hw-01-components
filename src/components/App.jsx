8bda1e29f4f66a7512fb901567de32708184be4c
romulopahuyo <romulo.pahuyo@gmail.com>
2024-07-07 19:39:06 +0800

    Updated components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,36 @@
+import { Profile } from './Profile/Profile';
+import user from '../data/user.json'; //IT IS THE DOWNLOADED JSON FORM THE TASK
+
+import { Statistics } from './Statistics/Statistics';
+import data from '../data/data.json'; //IT IS THE DOWNLOADED JSON FORM THE TASK
+
+import { FriendList } from './FriendList/FriendList';
+import friends from '../data/friends.json'; //IT IS THE DOWNLOADED JSON FORM THE TASK
+
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import transactions from '../data/transactions.json'; //IT IS THE DOWNLOADED JSON FORM THE TASK
+
+
 export const App = () => {
   return (
-    <div
+
+
+    //<> </> is Short Syntax for React Fragment instead of using parent element like div
+     <> 
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+        />
+        
+        <Statistics title="Upload Stats" stats={data} />
+        <FriendList friends={friends} />
+        <TransactionHistory items={transactions} />
+    </>
+  
+    /*<div
       style={{
         height: '100vh',
         display: 'flex',
@@ -12,5 +42,7 @@ export const App = () => {
     >
       React homework template
     </div>
+    */
   );
+
 };
